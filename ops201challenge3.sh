905a9d7e618fba3517b8d3df116b6a56eb1dbebf
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2022-11-10 19:54:21 -0700

    Create ops201challenge3.sh

---
+++ ops201challenge3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script:                       Ops Challenge 3
+# Author:                       Kevin Isaac
+# Date of latest revision:      20221110
+# Purpose:      Run a script and function to see who last logged in
+
+# Basic function
+print_something () {
+  echo Hello I am a function
+}
+print_something
+print_something
+
+fud=last
+$fud
