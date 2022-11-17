59c17343427f5648e359a8754ce1fc07f5630e7c
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2022-11-17 12:21:01 -0700

    Create opschallenge8.bat

---
+++ opschallenge8.bat
@@ -0,0 +1,8 @@
+@ECHO Off
+
+:: Script:                       Ops Challenge 8
+:: Author:                       Kevin Isaac
+:: Date of latest revision:      20221116
+:: Purpose:                     Run a Command Prompt Batch File
+ROBOCOPY /e "C:\Users\j.thompson\Desktop\My Project Files" "E:\J.Thompson Desktop Backups"
+
