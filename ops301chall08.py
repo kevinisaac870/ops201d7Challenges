b443422022c65b5f73245938cb5ad3f550fc4463
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2022-12-21 17:26:19 -0700

    Create ops301chall08.py

---
+++ ops301chall08.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Script:                       Ops Challenge 8
+# Author:                       Kevin Isaac
+# Date of latest revision:      20221221
+# Purpose:      Create a Python script to use lists & sets.
+
+
+# Assign to a variable a list of ten string elements.
+vari_list = ["dog", "cat", "ape", "monkey", "orca", "dolphin", "seal", "kangaroo", "chimp", "elephant"]
+
+
+# Print the fourth element of the list.
+print(vari_list[3])
+
+# Print the sixth through tenth element of the list.
+print(vari_list[5:10])
+# I got this to work, but do not understand how the square brackets work here in parenthesis but differently below.  I am reading up on this now.  Now that I am getting to
+# understand how this works (somewhat better), I am really liking it.
+# https://data-flair.training/blogs/python-data-structures-tutorial/
+
+
+# Change the value of the seventh element to "onion".
+vari_list[6] = "onion"
+print(vari_list)
+
+# End
