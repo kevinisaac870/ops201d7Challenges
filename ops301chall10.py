b1e1d14ddc726c1d62951396a2a54ab1bc54c7f7
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2023-01-13 14:33:57 -0700

    Update ops301chall10.py

--- ops301chall10.py
+++ ops301chall10.py
@@ -3,7 +3,7 @@
 # Script:                       Ops Challenge 10
 # Author:                       Kevin Isaac
 # Date of latest revision:      20221222
-# Purpose:     Using Pyhtion to amned a *.txt file
+# Purpose:     Using Pyhton to ammend a *.txt file
 
 
 # Importing Python OS
