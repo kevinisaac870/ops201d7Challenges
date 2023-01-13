75dd329297d7dc6c74a0bc669edaaeb798d34917
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2023-01-13 14:38:34 -0700

    Create ops301chall13.ps1

---
+++ ops301chall13.ps1
@@ -0,0 +1,10 @@
+# Script:                       Ops Challenge 13
+# Author:                       Kevin Isaac
+# Date of latest revision:      2022122
+# Purpose:     Utilizing Powershell AD Automation
+
+New-ADUser -Name "Franz Ferdinand" -City "Springfield" -State "Oregon" -OtherAttributes @{'title'="TPS Reporting Lead";'mail'="ferdi@GlobeXpower.com"}
+
+# End
+
+# Attribution:   Class Zoom video and fellow classmates
