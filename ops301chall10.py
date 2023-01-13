9745f0601e87bee42658c7d367ada9030695cbc7
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2023-01-13 09:14:55 -0700

    Create ops301chall10.py

---
+++ ops301chall10.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# Script:                       Ops Challenge 10
+# Author:                       Kevin Isaac
+# Date of latest revision:      20221222
+# Purpose:     Using Pyhtion to amned a *.txt file
+
+
+# Importing Python OS
+import os
+
+#Using file handling commands, create a Python script that creates a new .txt file
+
+f = open("kevintest.txt", "w")
+
+# Appends three lines
+
+line1 = "Merry\n"
+line2 = "Christmas\n"
+line3 = "To You all"
+
+f.write(line1)
+f.write(line2)
+f.write(line3)
+
+# Prints to the screen the first time
+
+f = open("kevintest.txt", "r")
+
+print(f.read())
+
+# Then delete the *.txt file
+
+os.remove("kevintest.txt")
+
+# End
+# Attribution: Class Zoom videos and class peers
