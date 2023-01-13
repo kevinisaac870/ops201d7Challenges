52db34946ec2781864a062214ebcbb8269e47d98
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2023-01-13 14:41:18 -0700

    Update ops301chal13.ps1

--- ops301chal13.ps1
+++ ops301chal13.ps1
@@ -1,1 +1,10 @@
-Under Construction - Awating stable interwebs...
+# Script:                       Ops Challenge 13
+# Author:                       Kevin Isaac
+# Date of latest revision:      2022122
+# Purpose:     Utilizing Powershell AD Automation
+
+New-ADUser -Name "Franz Ferdinand" -City "Springfield" -State "Oregon" -OtherAttributes @{'title'="TPS Reporting Lead";'mail'="ferdi@GlobeXpower.com"}
+
+# End
+
+# Attribution:   Class Zoom video and fellow classmates
